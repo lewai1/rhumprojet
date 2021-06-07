64beba59dd6f2d0de71ad0c1efaef0fb6cca5931
Guillaume <lukkin404@gmail.com>
2021-06-07 23:30:52 +0200

    GuyBrushProject
    Dossier de projet du code C# pour le programme de chiffrage et de déchiffrage de carte

---
+++ GuyBrushProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey yo World");
+
+            StreamWriter writer = new StreamWriter("../test.txt");
+            StreamWriter.Write("test de texte\nle retour à la ligne fonctionne-t-il ?\nBonne question...");
+            writer.Close();
+        }
+    }
+}
+
+
+// Proposer au lancement du programme de choisir le fichier à convertir parmi les .txt présents dans le dossier.
