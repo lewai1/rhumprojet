1280cacf3f09e6b78bdecf49e60b436b43756ceb
Guillaume <lukkin404@gmail.com>
2021-06-08 17:52:27 +0200

    Create Traduction.cs

---
+++ GuyBrushProject/Traduction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuyBrushProject
+{
+    public class Traduction
+    {
+        const string cheminTXT = ("../../../../Cartes/");
+        string ligne;
+        public void Dechiffrage(string nFile)
+        {
+            try
+            {
+                StreamReader lecture = new StreamReader(cheminTXT + nFile + ".chiffre");
+                StreamWriter ecriture = new StreamWriter(cheminTXT + nFile + ".clair");
+                ecriture.WriteLine(ecriture);
+                
+                lecture.Close(); ecriture.Close();
+            }
+            catch
+            {
+                Console.WriteLine("ERREUR : Fichier ne correspondant pas aux normes du traducteur.");
+            }
+        }
+    }
+}
