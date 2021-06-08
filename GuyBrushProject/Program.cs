aaf89d439e77c37e0e4f56ae40ee4115d1e2aab0
Guillaume <lukkin404@gmail.com>
2021-06-08 15:38:01 +0200

    Fonctionnement choix user V1.25
    pas totalement fonctionnel (dernière partie) mais tout nouveau fonctionnement (IEnumerable)

--- GuyBrushProject/Program.cs
+++ GuyBrushProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -9,25 +11,37 @@ namespace ConsoleApp1
         {
             int nbFichier = 1, choixUser;
             string cheminTXT = ("../../../../Cartes/");
-            string[] fichiers = Directory.GetFiles(cheminTXT, "*.txt");
+
+            IEnumerable<String> fichiers = Directory.EnumerateFiles(cheminTXT).Where(fichier => fichier.EndsWith(".clair") || fichier.EndsWith(".chiffre"));
+
             Console.WriteLine("Quel fichier voulez vous traduire ?\n");
             foreach (string fichier in fichiers)
             {
                 Console.WriteLine("{0} : {1}\n", nbFichier++, (fichier.Substring(fichier.LastIndexOf("/") + 1)));
             }
             Console.Write("Choix : ");
-            choixUser = Convert.ToInt32(Console.ReadLine()) - 1; // -1 pour faire correspondre le choix du joueur avec la numérotation de la liste fichiers
 
-            while ((choixUser < 0) || (choixUser > nbFichier - 2))
+            try
             {
-                Console.Write("Veuillez faire un choix valide : ");
-                choixUser = Convert.ToInt32(Console.ReadLine()) - 1;
+                choixUser = Convert.ToInt32(Console.ReadLine()) - 1; // -1 pour faire correspondre le choix du joueur avec la numérotation de la liste fichiers
+                while ((choixUser < 0) || (choixUser > nbFichier - 2))
+                {
+                    Console.Write("Veuillez faire un choix valide : ");
+                    choixUser = Convert.ToInt32(Console.ReadLine()) - 1;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("\nERREUR : Valeur non valide");
             }
 
 
-            if (fichiers[choixUser].Contains(".clair")) Console.WriteLine("ok");
-            else if (fichiers[choixUser].Contains(".chiffre")) Console.WriteLine("ko");
-            else Console.WriteLine("ni ko ni ok");
+            foreach (string fichier in fichiers)
+            {
+                if (fichier.EndsWith(".clair")) Console.WriteLine("ok");
+                else if (fichier.EndsWith(".chiffre")) Console.WriteLine("ko");
+                else Console.WriteLine("soucis !");
+            }
 
 
 
